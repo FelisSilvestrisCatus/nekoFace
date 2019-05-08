f9e86bb86b56f5f38116547a51125430639e8967
LinLinLinWang <1339567630@qq.com>
2019-05-08 14:28:12 +0800

    项目搬迁

--- src/main/java/nekoFace/Face.java
+++ src/main/java/nekoFace/Face.java
@@ -26,57 +26,7 @@ public class Face {
 //        System.load("C:\\vfiles\\opencv\\opencv_java401.dll");
 //    }
 
-    public static int getPhotoNum(String uid) {
-        File file = new File("C:\\vfiles\\photo\\" + uid);
-        String files[];
-        files = file.list();
-        int num = files.length;
 
-        return num;
-    }
-
-    /**
-     * @param img      ：前端request 获得的图像数据
-     * @param uid：用户id
-     * @return
-     */
-    public static String base64StrToImage(String img, String uid) {
-        SimpleDateFormat f = new SimpleDateFormat("yyyyMMddHHmmss");
-        String dirpath = "C:\\vfiles\\photo_login\\" + uid;
-        File dirFile = new File(dirpath);
-        if (!dirFile.exists()) {
-            dirFile.mkdirs();
-        }
-        String imgStr = img.substring(22, img.length());
-
-        if (imgStr == null)
-            return "";
-        BASE64Decoder decoder = new BASE64Decoder();
-        String filename = dirpath + "\\" + f.format(new Date()) + ".png";
-        try {
-            // 解密
-            byte[] b = decoder.decodeBuffer(imgStr);
-            // 处理数据
-            for (int i = 0; i < b.length; ++i) {
-                if (b[i] < 0) {
-                    b[i] += 256;
-                }
-            }
-            // 文件夹不存在则自动创建
-
-            File tempFile = new File(filename);
-            if (!tempFile.getParentFile().exists()) {
-                tempFile.getParentFile().mkdirs();
-            }
-            OutputStream out = new FileOutputStream(tempFile);
-            out.write(b);
-            out.flush();
-            out.close();
-            return filename;
-        } catch (Exception e) {
-            return filename;
-        }
-    }
 
     //检测人脸  //若检测到 进行裁剪 并返回值
 
@@ -233,7 +183,99 @@ public class Face {
 
         return flag;
     }
+//    //模型训练 用户上传完照片后自动调用模型训练  将生成后的模型保存 后期直接使用
+//    public static void train() throws IOException {
+//        //模型训练之前 生成csv文件
+//        Face.getCsv();
+//        String trainingDir = "C:\\vfiles\\a.text";
+//
+//
+//        // 封装数据源
+//        BufferedReader br = new BufferedReader(new FileReader(trainingDir));
+//        // 封装目的地(创建集合对象)
+//        ArrayList<String> array = new ArrayList<String>();
+//
+//        // 读取数据存储到集合中
+//        String line = null;
+//        while ((line = br.readLine()) != null) {
+//            array.add(line);
+//        }
+//
+//        // 释放资源
+//        br.close();
+//        MatVector images = new MatVector(array.size());
+//
+//        Mat labels = new Mat(array.size(), 1, CV_32SC1);
+//        IntBuffer labelsBuf = labels.createBuffer();
+//        int counter = 0;
+//        System.out.println(array.size());
+//        for (String s : array) {
+//
+//
+//            Mat img = imread(s.split(";")[0], Imgcodecs.IMREAD_GRAYSCALE);
+//
+//            System.out.println("加载" + counter + "张" + "像素" + img.arrayHeight() * img.arrayWidth());
+//            System.out.println(counter + "张" + "通道数目" + img.channels());
+//            int label = Integer.parseInt(s.split(";")[1]);
+//            System.out.println(label + "label");
+//            images.put(counter, img);
+//
+//            labelsBuf.put(counter, label);
+//
+//            counter++;
+//        }
+//
+//        FaceRecognizer faceRecognizer = FisherFaceRecognizer.create();
+//        // FaceRecognizer faceRecognizer = EigenFaceRecognizer.create();
+//        // FaceRecognizer faceRecognizer = LBPHFaceRecognizer.create();
+//        System.out.println("模型标签的数目" + labels.size());
+//        System.out.println("模型开始训练 ");
+//        faceRecognizer.train(images, labels);
+//
+//        //   IntPointer label = new IntPointer(1);
+//        //  DoublePointer confidence = new DoublePointer(1);
+//        //faceRecognizer.predict(testImage, label, confidence);
+//        //   int predictedLabel = label.get(0);
+////
+//        System.out.println("模型训练完毕 ");
+//        faceRecognizer.save("C:\\vfiles\\Model.xml");
+//        System.out.println("模型保存完毕 ");
+//
+//    }
 
+//    //人脸识别
+//    //学生用来登录或者点名时所需（）
+//    public static Map<String, String> validate(String uid, String img) {
+//
+//        boolean flag = false;
+//        //将照片字节码转为指定格式的照片  若通过返回值
+//        String loginimgpath = Face.base64StrToImage(img, uid);
+//        //识别人脸并裁剪
+//        Map<String, String> map = new HashMap<>();
+//        map = Face.facedetection(loginimgpath, uid, "1");// 1 表示该该方法用来登录存放临时的图片
+//        if (!(map.get("flag").equalsIgnoreCase("1"))) {
+//            map.put("isThisGuy", "no");//也同时拒绝他的登录  或者这个哔是冒名顶替的
+//            return map;// 不具备人脸识别的条件
+//        }
+//        //开始人脸识别
+//        //加载模型
+//        FaceRecognizer faceRecognizer = FisherFaceRecognizer.create();
+//
+//        IntPointer label = new IntPointer(1);
+//        DoublePointer confidence = new DoublePointer(1);
+//        faceRecognizer.setThreshold(94.22);//设置阈值
+//        faceRecognizer.read("C:\\vfiles\\Model.xml");
+//        //图像灰度化
+//        Mat Image = imread(map.get("path"), Imgcodecs.IMREAD_GRAYSCALE);
+//        faceRecognizer.predict(Image, label, confidence);
+//        int predictedLabel = label.get(0);
+//        if (predictedLabel == Integer.valueOf(uid)) {
+//            map.put("isThisGuy", "yes"); //是他就是他
+//            return map;
+//        }
+//        map.put("isThisGuy", "no");//也同时拒绝他的登录  或者这个哔是冒名顶替的
+//        return map;
+//    }
 
 }
 
